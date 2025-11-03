42707bf0ea111f28c547ee26c27d79be9c51502e
murodov109 <akabekov77@gmail.com>
2025-11-03 23:38:09 +0500

    Update api_server.py

--- api_server.py
+++ api_server.py
@@ -1,10 +1,9 @@
-from flask import Flask, jsonify
-
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"status": "Bot running successfully!"})
+    return "Bot is running!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+def start_server():
+    app.run(host="0.0.0.0", port=8080)

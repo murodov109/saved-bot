e63ab6be8d2462194407d71ecb22884023d7e80b
murodov109 <akabekov77@gmail.com>
2025-11-03 17:49:27 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,4 @@
-import os
-
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8435705646:AAEJULzHr7N2Z-lleYsoQKf36yVnHxd0iXU")
-ADMIN_ID = int(os.getenv("ADMIN_ID", 7617397626))
-
-MANDATORY_CHANNELS = [
-    "@your_channel_1",
-    "@your_channel_2"
-]
-
-DOWNLOAD_PATH = "downloads"
-
-MAX_FILE_SIZE_MB = 100
+BOT_TOKEN = "8435705646:AAEJULzHr7N2Z-lleYsoQKf36yVnHxd0iXU"
+ADMINS = [7617397626]
+CHANNELS = ["@kanal_nomi1", "@kanal_nomi2"]
+DB_FILE = "database.sqlite"

26268fea4e1e3aba5cd161617542535eacb029dd
murodov109 <akabekov77@gmail.com>
2025-11-03 22:18:57 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,2 @@
-import os
-
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8435705646:AAEJULzHr7N2Z-lleYsoQKf36yVnHxd0iXU")
+BOT_TOKEN = "8435705646:AAEJULzHr7N2Z-lleYsoQKf36yVnHxd0iXU"
 ADMINS = [7617397626]
-API_URL = os.getenv("API_URL", "https://api.ssscrap.com/download?url=")
